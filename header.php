a7f3b1b8de4e1621910756fbf34b26d1d05f73dd
Andrew Minion <andrew@andrewrminion.com>
2014-09-10 12:15:13 -0400

    style header logo

--- header.php
+++ header.php
@@ -34,7 +34,6 @@
 	<div id="page" class="hfeed site">
 		<header id="masthead" class="site-header" role="banner">
 			<a class="home-link" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-				<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/church-header.jpg" alt="Photo of Northside Baptist Church" title="Northside Baptist Church | Moncks Corner, SC" width="100%" />
 			    <img class="logo" src="<?php echo get_stylesheet_directory_uri(); ?>/images/Northside-Baptist-Church-logo.svg" title="Northside Baptist Church" alt="Northside Baptist Church logo" />
 				<h1 class="site-title"><?php bloginfo( 'name' ); ?></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
