1a53f2fc39e006b65949e12aefd1e845a27ab6e9
Andrew Minion <andrew@andrewrminion.com>
2014-09-16 11:31:21 -0400

    change fonts

--- functions.php
+++ functions.php
@@ -3,4 +3,10 @@
 include('functions-branding.php');
 include('functions-church.php');
 
+/* deregister Bitter and Open Sans webfonts */
+function deregister_default_fonts() {
+    wp_deregister_style( 'twentythirteen-fonts' );
+}
+add_action( 'wp_enqueue_scripts', 'deregister_default_fonts', 100 );
+
 ?>
