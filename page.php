89a176bdb8f27c5c4cd7d2f7e2e12ebdc4b106d7
Andrew Minion <andrew@andrewrminion.com>
2014-10-02 09:39:33 -0400

    remove entry-meta to fix a spacing issue

--- page.php
+++ page.php
@@ -37,9 +37,6 @@ get_header(); ?>
 						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
 					</div><!-- .entry-content -->
 
-					<footer class="entry-meta">
-						<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-					</footer><!-- .entry-meta -->
 				</article><!-- #post -->
 
 				<?php comments_template(); ?>
