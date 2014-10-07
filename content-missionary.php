e99935713deb4af738990052a0b087eee583b5d7
Andrew Minion <andrew@andrewrminion.com>
2014-10-07 15:53:14 -0400

    tweak missionary archive page

--- content-missionary.php
+++ content-missionary.php
@@ -11,22 +11,22 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php if ( has_post_thumbnail() && ! post_password_required() && ! is_attachment() && is_single() ) : ?>
-		<div class="entry-thumbnail">
-			<?php the_post_thumbnail(); ?>
-		</div>
-		<?php endif; ?>
+    <?php endif; // is_single() ?>
+        <header class="entry-header">
+            <?php if ( has_post_thumbnail() && ! post_password_required() && ! is_attachment() && is_single() ) : ?>
+            <div class="entry-thumbnail">
+                <?php the_post_thumbnail(); ?>
+            </div>
+            <?php endif; ?>
 
-		<?php if ( is_single() ) : ?>
-		<h1 class="entry-title"><?php the_title(); ?></h1>
-		<?php endif; // is_single() ?>
+            <h1 class="entry-title"><?php the_title(); ?></h1>
 
-		<div class="entry-meta">
-			<?php twentythirteen_entry_meta(); ?>
-			<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+            <div class="entry-meta">
+                <?php twentythirteen_entry_meta(); ?>
+                <?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
+            </div><!-- .entry-meta -->
+        </header><!-- .entry-header -->
+    <?php endif; // is_single() ?>
 
 	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
 	<div class="entry-summary">
