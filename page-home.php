4529050b54bd3c23f199559fc2056d0689300eb4
Andrew Minion <andrew@andrewrminion.com>
2014-10-18 15:24:11 -0400

    restructure homepage
    layout positioning
    minor text styles
    quick link image size
    add meteor slideshow and quick links via function call in page-home.php rather than shortcode in page content
    tweak slide caption size

--- page-home.php
+++ page-home.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that other
- * 'pages' on your WordPress site will use a different template.
+ * The template for displaying the home page
  *
  * @package WordPress
  * @subpackage Twenty_Thirteen
@@ -20,22 +16,16 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                    <?php if ( ! is_front_page() ) : ?>
-                        <header class="entry-header">
-                            <?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
-                            <div class="entry-thumbnail">
-                                <?php the_post_thumbnail(); ?>
-                            </div>
-                            <?php endif; ?>
 
-                            <h1 class="entry-title"><?php the_title(); ?></h1>
-                        </header><!-- .entry-header -->
-                    <?php endif; ?>
+                    <div class="home-top-row">
+                        <?php if ( function_exists( 'meteor_slideshow' ) ) { meteor_slideshow( "home" ); } ?>
 
-					<div class="entry-content">
-						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-					</div><!-- .entry-content -->
+                        <div class="entry-content">
+                            <?php the_content(); ?>
+                        </div><!-- .entry-content -->
+                    </div><!-- .home-top-row -->
+
+					<?php if ( function_exists( 'home_quick_links' ) ) { home_quick_links(); } ?>
 
 				</article><!-- #post -->
 
